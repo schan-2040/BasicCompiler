4a2fd5a479fa997f6e81056d79afcddc4f039927
Shaurya Chandhoke <38062430+schan-2040@users.noreply.github.com>
2018-09-18 21:17:41 -0400

    Update globalvar.h

--- globalvar.h
+++ globalvar.h
@@ -11,4 +11,5 @@ extern std::vector<Token> tokenCollector;
 extern string typeInString(int enumeratorVal);
 extern void start();
 extern std::vector< std::vector<Token> > SLIST;
+
 #endif // GLOBALVAR_H_INCLUDED
